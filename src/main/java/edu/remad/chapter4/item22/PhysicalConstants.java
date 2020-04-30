322ca6defbf460e002ae92c56b752affc4f85a2e
RenatiusvanSander / R. Meier <remad@web.de>
2020-04-30 20:50:42 +0200

    EffectiveJava: added chapter 4 item 21

---
+++ src/main/java/edu/remad/chapter4/item22/PhysicalConstants.java
@@ -0,0 +1,10 @@
+package edu.remad.chapter4.item22;
+
+public class PhysicalConstants {
+
+    private PhysicalConstants() {} // Prevents instantiation.
+
+    public static final double AVOGADROS = 6.022_140_857e23;
+    public static final double BOLTZMANN_CONST = 1.380_648_52e-23;
+    public static final double ELECTRON_MASS = 9.109_383_56e-31;
+}
