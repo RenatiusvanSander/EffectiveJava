ecf4b01e5d53d170ee6b5089e1a44a83a943a671
RenatiusvanSander / R. Meier <remad@web.de>
2020-05-01 20:39:30 +0200

    EffectiveJava: Cretaed Junit Tests for MySet

--- src/main/java/edu/remad/chapter4/item24/MySet.java
+++ src/main/java/edu/remad/chapter4/item24/MySet.java
@@ -2,6 +2,7 @@ package edu.remad.chapter4.item24;
 
 import java.util.AbstractSet;
 import java.util.Iterator;
+import java.util.Set;
 import java.util.Spliterator;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
@@ -9,36 +10,45 @@ import java.util.stream.Stream;
 
 public class MySet<E> extends AbstractSet<E> {
 
-    private MyIterator iterator = new MyIterator();
+    public MySet() {
+        myIterator = new MyIterator(iterator());
+    }
+
+    public MySet(Set<E> set) {
+        this.addAll(set);
+        myIterator = new MyIterator(iterator());
+    }
+
+    private final MyIterator myIterator;
 
     @Override
     public Iterator<E> iterator() {
-        return iterator;
+        return myIterator;
     }
 
     @Override
     public void forEach(Consumer<? super E> action) {
-
+        super.forEach(action);
     }
 
     @Override
     public boolean removeIf(Predicate<? super E> filter) {
-        return false;
+        return super.removeIf(filter);
     }
 
     @Override
     public Spliterator<E> spliterator() {
-        return null;
+        return super.spliterator();
     }
 
     @Override
     public Stream<E> stream() {
-        return null;
+        return super.stream();
     }
 
     @Override
     public Stream<E> parallelStream() {
-        return null;
+        return super.parallelStream();
     }
 
     @Override
@@ -46,16 +56,22 @@ public class MySet<E> extends AbstractSet<E> {
         return 0;
     }
 
-    private class MyIterator implements  Iterator<E> {
+    private class MyIterator implements Iterator<E> {
+
+        public MyIterator(Iterator<E> iterator) {
+            this.iterator = iterator;
+        }
 
         @Override
         public boolean hasNext() {
-            return false;
+            return iterator.hasNext();
         }
 
         @Override
         public E next() {
-            return null;
+            return iterator.next();
         }
+
+        private Iterator<E> iterator = null;
     }
 }
